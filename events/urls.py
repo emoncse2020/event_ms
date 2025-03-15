314db338b15f4e538c1e56dc7de7ecaeaf12c6c1
emoncse2020 <emon.cse.2020@gmail.com>
2025-03-16 03:02:15 +0600

    default page

--- events/urls.py
+++ events/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import home,create_event, organizer_dashboard, view_events, update_event, delete_event
 
 urlpatterns = [
-    path('home/', home, name='home'),
+    path('', home, name='home'),
     path('create-event/', create_event, name='create-event'),
     path('update-event/<int:id>/', update_event, name='update-event'),
     path('delete-event/<int:id>/', delete_event, name="delete-event"),
